92249290022003c8124e7bbd2e0692bd3e6cf51d
Joey Kudish <joachim.kudish@gmail.com>
2023-11-26 19:23:09 -0800

    output to csv, json or table format
    Refactor the export to provide different outputs for CSV file, JSON file or default table format

--- includes/edd-export-command.php
+++ includes/edd-export-command.php
@@ -18,17 +18,39 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 		 *   - json
 		 * ---
 		 *
+		 * [--destination=<destination>]
+		 * : Path to the destination directory for the resulting CSV or JSON file. Defaults to the wp-uploads/edd-exports folder.
+		 *
 		 * @param array $args Positional arguments.
 		 * @param array $assoc_args Associative arguments.
 		 */
 		public function payments( $args, $assoc_args ) {
 			$this->version_check();
+			$args = wp_parse_args( $assoc_args, array(
+				'output-format' => 'table',
+				'destination'   => wp_upload_dir()['basedir'] . '/edd-exports',
+			) );
+
 			$payments_data = $this->get_payments_data();
-			if (empty($payments_data)) {
+			if ( empty( $payments_data ) ) {
 				return WP_CLI::error( __( 'No EDD payments found.', 'edd-export-tool' ) );
 			}
 
-			return WP_CLI\Utils\format_items( $assoc_args['output-format'], $payments_data, array_keys( $payments_data[0] ) );
+			switch ( $args['output-format'] ) {
+				case 'csv':
+					$file_path = $this->write_csv( $payments_data, $args['destination'] );
+
+					return WP_CLI::success( sprintf( '%s %s', __( 'Payments exported to CSV file:', 'edd-export-tool' ), $file_path ) );
+
+				case 'json':
+					$file_path = $this->write_json( $payments_data, $args['destination'] );
+
+					return WP_CLI::success( sprintf( '%s %s', __( 'Payments exported to JSON file:', 'edd-export-tool' ), $file_path ) );
+
+				case 'table':
+				default:
+					return WP_CLI\Utils\format_items( 'table', $payments_data, array_keys( $payments_data[0] ) );
+			}
 		}
 
 		/**
@@ -55,6 +77,7 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			}
 
 			$data = apply_filters( 'edd_export_tool_get_payments_data', $data );
+
 			return $data;
 		}
 
@@ -81,7 +104,72 @@ if ( defined( 'WP_CLI' ) && WP_CLI ) {
 			);
 
 			$payment_data = apply_filters( 'edd_export_tool_get_payment_data', $payment_data );
+
 			return $payment_data;
+		}
+
+		/**
+		 * Write the CSV file
+		 *
+		 * @param array $data The data to write to the CSV file
+		 * @param string $destination The path to the destination directory for the resulting CSV file
+		 *
+		 * @return mixed WP_CLI::error if the file could not be opened for writing, otherwise the path to the CSV file
+		 * @since 1.0
+		 */
+		private function write_csv( $data, $destination ) {
+			$file_path = trailingslashit( $destination ) . 'edd-payments-' . date( 'Y-m-d' ) . '.csv';
+			$file      = $this->get_file( $file_path );
+			if ( ! $file ) {
+				return WP_CLI::error( sprintf( '%s %s', __( 'Could not open csv file for writing:', 'edd-export-tool' ), $file_path ) );
+			}
+
+			WP_CLI\Utils\write_csv( $file, $data, array_keys( $data[0] ) );
+			return $file_path;
+		}
+
+		/**
+		 * Write the JSON file
+		 *
+		 * @param array $data The data to write to the CSV file
+		 * @param string $destination The path to the destination directory for the resulting CSV file
+		 *
+		 * @return mixed WP_CLI::error if the file could not be opened for writing, otherwise the path to the JSON file
+		 * @since 1.0
+		 */
+		private function write_json( $data, $destination ) {
+			$file_path = trailingslashit( $destination ) . 'edd-payments-' . date( 'Y-m-d' ) . '.json';
+			$file      = $this->get_file( $file_path );
+			if ( ! $file ) {
+				return WP_CLI::error( sprintf( '%s %s', __( 'Could not open json file for writing:', 'edd-export-tool' ), $file_path ) );
+			}
+
+			$json = json_encode( $data, JSON_PRETTY_PRINT );
+			file_put_contents( $file_path, $json );
+			return $file_path;
+		}
+
+		/**
+		 * Retrieve the file data is written to
+		 *
+		 * @param string $file_path The path to the file
+		 *
+		 * @return false|resource file pointer resource on success, or false on error.
+		 * @since 1.0
+		 */
+		private function get_file( $file_path ) {
+			$dir = dirname( $file_path );
+			if ( ! is_dir( $dir ) ) {
+				@mkdir( $dir, 0775, true ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+			}
+
+
+			if ( ! @file_exists( $file_path ) ) {
+				@file_put_contents( $file_path, '' ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+				@chmod( $file_path, 0664 ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
+			}
+
+			return @fopen( $file_path, 'w' ); // phpcs:ignore WordPress.PHP.NoSilencedErrors.Discouraged
 		}
 
 		/**
